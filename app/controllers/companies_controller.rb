335f65e6cecd786b5429d77f8a8af8e5b8eadbaa
Cameron Webster <cameron.webster@gmail.com>
2015-02-25 14:20:37 -0700

    user can create company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,17 @@ class CompaniesController < ApplicationController
     @company = Company.find(params[:id])
   end
 
+  def new
+    @company = Company.new
+  end
+
+
+  def create
+    @company = Company.create(params.require(:company).permit(:name, :catch_phrase, :suffix))
+    if @company.save
+      redirect_to company_path(@company)
+    else
+      render :new
+    end
+  end
 end
